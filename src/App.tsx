aa0cf691445330ed9adad17be634d743a9b6518a
yankvasya <yankvasya@gmail.com>
2022-08-13 16:56:15 +0300

    feat(Card): created component and added props typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card, { cardBorder } from "./components/Card";
 
-function App() {
+const App = () => {
+  const num = 1;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card
+        onClick={(num) => console.log(num)}
+        width="60px"
+        height="60px"
+        border={cardBorder.light}
+      >
+        <button>1231</button>
+        <button>1231</button>
+      </Card>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,51 @@
+import React, { FC, PropsWithChildren, useState } from "react";
+
+export enum cardBorder {
+  border = "border",
+  light = "light",
+  none = "none",
+}
+
+interface cardProps {
+  width: string;
+  height: string;
+  padding?: string;
+  border: cardBorder;
+  onClick: (num: number) => void;
+}
+
+const Card: FC<PropsWithChildren<cardProps>> = ({
+  height,
+  padding,
+  width,
+  border,
+  onClick,
+  children,
+}) => {
+  const [state, setState] = useState(0);
+  return (
+    <div
+      onClick={() => {
+        onClick(state);
+        setState(state + 1);
+      }}
+      style={{
+        width,
+        height,
+        padding,
+        border:
+          border === cardBorder.border
+            ? "1px solid red"
+            : border === cardBorder.light
+            ? "2px solid yellow"
+            : "none",
+        background: "gray",
+      }}
+    >
+      card
+      {children}
+    </div>
+  );
+};
+
+export default Card;
