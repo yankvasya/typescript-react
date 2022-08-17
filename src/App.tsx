e7649439370ff9f9cdd05569b38583f25d607d11
yankvasya <yankvasya@gmail.com>
2022-08-17 23:46:08 +0300

    feat(App): added fetch users API and typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card, { cardBorder } from "./components/Card";
 import UserList from "./components/UserList";
 import { IUser } from "./types/types";
+import axios from "axios";
 
 const App = () => {
-  const users: IUser[] = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "Sincere@april.biz",
-      address: {
-        street: "Kulas Light",
-        suite: "Apt. 556",
-        city: "Gwenborough",
-        zipcode: "92998-3874",
-        geo: {
-          lat: "-37.3159",
-          lng: "81.1496",
-        },
-      },
-    },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      username: "Antonette",
-      email: "Shanna@melissa.tv",
-      address: {
-        street: "Kulas Light",
-        suite: "Apt. 556",
-        city: "Gwenborough",
-        zipcode: "92998-3874",
-        geo: {
-          lat: "-37.3159",
-          lng: "81.1496",
-        },
-      },
-      phone: "010-692-6593 x09125",
-      website: "anastasia.net",
-      company: {
-        name: "Deckow-Crist",
-        catchPhrase: "Proactive didactic contingency",
-        bs: "synergize scalable supply-chains",
-      },
-    },
-  ];
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = (
+        await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users")
+      ).data;
+
+      setUsers(response);
+    } catch (error) {
+      throw new Error((error as Error).message);
+    }
+  };
 
   return (
     <div>
